77c1ed39822ddeb1569e28de0fb23f1b878324c3
Dominik <me@domi-btnr.dev>
2023-06-04 20:32:35 +0200

    Update Age.jsx

--- src/components/Age.jsx
+++ src/components/Age.jsx
@@ -6,17 +6,11 @@ const getAge = (birthDate) => {
     const yearsDiff = Math.floor(ageDiff / (1000 * 60 * 60 * 24 * 365));
     const monthsDiff = currentDate.getMonth() - birthDate.getMonth();
     const daysDiff = currentDate.getDate() - birthDate.getDate();
-    const hoursDiff = currentDate.getHours() - birthDate.getHours();
-    const minutesDiff = currentDate.getMinutes() - birthDate.getMinutes();
-    const secondsDiff = currentDate.getSeconds() - birthDate.getSeconds();
 
     return {
         years: yearsDiff,
         months: yearsDiff * 12 + monthsDiff,
-        days: yearsDiff * 365 + Math.floor(monthsDiff * 30.44) + daysDiff,
-        hours: yearsDiff * 365 * 24 + Math.floor(monthsDiff * 30.44 * 24) + daysDiff * 24 + hoursDiff,
-        minutes: yearsDiff * 365 * 24 * 60 + Math.floor(monthsDiff * 30.44 * 24 * 60) + daysDiff * 24 * 60 + hoursDiff * 60 + minutesDiff,
-        seconds: yearsDiff * 365 * 24 * 60 * 60 + Math.floor(monthsDiff * 30.44 * 24 * 60 * 60) + daysDiff * 24 * 60 * 60 + hoursDiff * 60 * 60 + minutesDiff * 60 + secondsDiff,
+        days: yearsDiff * 365 + Math.floor(monthsDiff * 30.44) + daysDiff
     };
 };
 
