270efeebf316572ce2c6e45a9b58719fbad49757
Dominik <me@domi-btnr.dev>
2024-05-28 09:23:01 +0000

    Switch to createResource

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,39 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faCodeFork, faStar, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate, faCodeFork, faStar, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import Fa from "solid-fa";
-import { createEffect, createSignal, For, onCleanup, Show } from "solid-js";
+import { createResource, For, Match, onCleanup, Show, Switch } from "solid-js";
 
 import Style from "./Projects.module.scss";
 import SocialButton from "./SocialButton";
 
 const ProjectTile = props => {
+    const retryTimeout = null;
+
+    const fetchLanguages = async url => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (!response.ok) {
+            const resetTimestamp = response.headers.get("X-Ratelimit-Reset");
+            const remainingRequests = Number(response.headers.get("X-Ratelimit-Remaining") ?? 0);
+            const errorMessage = `API request failed with status ${response.status}: ${data.message.split(". ")[0]}`;
+            if (!remainingRequests) {
+                const resetTime = new Date(resetTimestamp * 1000);
+                const delay = resetTime - new Date();
+                const prettyResetTime = `${resetTime.getHours().toString().padStart(2, "0")}:${resetTime.getMinutes().toString().padStart(2, "0")}`;
+                console.error(`Rate limit exceeded, retrying at ${prettyResetTime}`);
+                setTimeout(refetch, delay);
+            } else console.error(errorMessage);
+            throw new Error(errorMessage);
+        }
+
+        return Object.keys(data).slice(0, 4);
+    };
+
     // eslint-disable-next-line solid/reactivity
-    const [languages, setLanguages] = createSignal([props.language]);
-    createEffect(() => {
-        fetch(props.languages_url)
-            .then(response => response.json())
-            .then(data => Object.keys(data))
-            .then(data => data.slice(0, 4))
-            .then(data => setLanguages(data))
-            .catch(error => console.error(error));
-    });
+    const [languages, { refetch }] = createResource(fetchLanguages, { initialValue: [props.language] });
+    onCleanup(() => clearTimeout(retryTimeout));
+
     return (
         <div class={Style.projectTile}>
             <h3>{props.name}</h3>
@@ -50,60 +67,58 @@ const ProjectTile = props => {
 
 const Projects = () => {
     const GH_USER = "domi-btnr";
-    const [projects, setProjects] = createSignal([]);
-    const [error, setError] = createSignal(null);
     let retryTimeout = null;
 
-    const fetchProjects = () => {
-        fetch(`https://api.github.com/users/${GH_USER}/repos`)
-            .then(response => {
-                const resetTimestamp = response.headers.get("x-ratelimit-reset");
-                return response.json().then(data => {
-                    if (!response.ok) {
-                        const errorMessage = `API request failed with status ${response.status}: ${data.message.split(". ")[0]}`;
-                        if (response.status === 403 && resetTimestamp) {
-                            const resetTime = new Date(resetTimestamp * 1000);
-                            const delay = resetTime - new Date();
-                            console.log(`Rate limit exceeded, retrying at ${resetTime}`);
-                            clearTimeout(retryTimeout);
-                            retryTimeout = setTimeout(fetchProjects, delay);
-                        }
-                        throw new Error(errorMessage);
-                    }
-                    return data;
-                });
-            })
-            .then(data => {
-                if (!Array.isArray(data)) throw new TypeError(`Expected an array but got ${typeof data}`);
-                return data.filter(repo => !repo.fork && !repo.archived && repo.name !== GH_USER)
-                    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-            })
-            .then(data => {
-                setProjects(data);
-                setError(null);
-            })
-            .catch(error => {
-                setError(error.message);
-                console.error(error.message);
-            });
+    const fetchProjects = async () => {
+        const response = await fetch(`https://api.github.com/users/${GH_USER}/repos`);
+        const data = await response.json();
+
+        if (!response.ok) {
+            const resetTimestamp = response.headers.get("X-Ratelimit-Reset");
+            const remainingRequests = Number(response.headers.get("X-Ratelimit-Remaining") ?? 0);
+            const errorMessage = `API request failed with status ${response.status}: ${data.message.split(". ")[0]}`;
+            if (!remainingRequests) {
+                const resetTime = new Date(resetTimestamp * 1000);
+                const delay = resetTime - new Date();
+                const prettyResetTime = `${resetTime.getHours().toString().padStart(2, "0")}:${resetTime.getMinutes().toString().padStart(2, "0")}`;
+                console.error(`Rate limit exceeded, retrying at ${prettyResetTime}`);
+                if (retryTimeout) clearTimeout(retryTimeout);
+                retryTimeout = setTimeout(refetch, delay);
+            } else console.error(errorMessage);
+            throw new Error(errorMessage);
+        }
+
+        if (!Array.isArray(data)) throw new TypeError(`Expected an array but got ${typeof data}`);
+
+        return data.filter(repo => !repo.fork && !repo.archived && repo.name !== GH_USER)
+            .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
     };
 
-    createEffect(() => fetchProjects());
+    const [projects, { refetch }] = createResource(fetchProjects, { initialValue: [] });
     onCleanup(() => clearTimeout(retryTimeout));
 
     return (
         <div class={Style.projectList}>
-            <Show when={error()} fallback={
-                <For each={projects()}>
-                    {project => <ProjectTile {...project} />}
-                </For>
-            }>
-                <div class={Style.errorWrapper}>
-                    <Fa icon={faTriangleExclamation} />
-                    <span>Error fetching projects:</span>
-                    <p>{error()}</p>
-                </div>
-            </Show>
+            <Switch>
+                <Match when={projects.loading}>
+                    <div class={Style.loadingWrapper}>
+                        <Fa icon={faArrowsRotate} />
+                        <span>Loading projects...</span>
+                    </div>
+                </Match>
+                <Match when={projects.error}>
+                    <div class={Style.errorWrapper}>
+                        <Fa icon={faTriangleExclamation} />
+                        <span>Error fetching projects:</span>
+                        <p>{projects.error.message}</p>
+                    </div>
+                </Match>
+                <Match when={projects()}>
+                    <For each={projects()}>
+                        {project => <ProjectTile {...project} />}
+                    </For>
+                </Match>
+            </Switch>
         </div>
     );
 };
